668bff3b17c0fa19c358fb68c9413a5e5aafeb88
Yash Khare <60147732+khareyash05@users.noreply.github.com>
2021-12-21 20:11:07 +0530

    storing data to a test file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const cors = require("cors");
+const fs = require('fs');
 
 app.use(express.json())
 app.use(cors());
 app.post("/",(req,res)=>{
+    fs.writeFileSync('test.js',req.body.code);
     console.log(req.body);
     res.json({message : "sucess"});
 })
