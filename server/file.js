c4541ed5fdbb9ff1aa7039dcf08efd9c95ebba12
Yash Khare <60147732+khareyash05@users.noreply.github.com>
2021-12-23 18:59:06 +0530

    introduced python script

--- server/file.js
+++
@@ -1,3 +0,0 @@
-let a=0
-let b=1
-console.log(a+b)
--- server/server.js
+++ server/server.js
@@ -2,12 +2,25 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const fs = require('fs');
+const {PythonShell} =require('python-shell');
 
 app.use(express.json())
 app.use(cors());
 app.post("/",(req,res)=>{
-    fs.writeFileSync('file.js',req.body.code);
+    fs.writeFileSync('python.py',req.body.code);
     console.log(req.body);
+    let options = {
+        mode: 'text',
+        pythonOptions: ['-u'], // get print results in real-time          
+        args: ['shubhamk314'] //An argument which can be accessed in the script using sys.argv[1]
+    };   
+    console.log("ffjgf");
+    PythonShell.run('python.py', options, function (err, result){
+          if (err) console.log(err);
+          console.log('result: ', result.toString());
+          res.send(result.toString())
+    });
+    console.log("fgjuh")
     res.json({message : "sucess"});
 })
 app.listen(5000,()=>{
